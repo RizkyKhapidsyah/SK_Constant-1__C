c00b8001da3b1265671931075035717a0e1d7b4e
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2020-11-05 20:40:08 +0700

    Add project files.

---
+++ SK_Constant-1__C/Source.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+
+int main()
+{
+	int row, column;
+
+	srand((unsigned)time(NULL));
+
+	for (row = 0; row < 10; row++)
+	{
+		for (column = 0; column < 10; column++) {
+			printf("%2d\t", rand() % 100);
+		}
+
+		putchar('\n');
+	}
+
+	_getch();
+	return(0);
+}
